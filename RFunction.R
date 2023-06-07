ff2cc93ac2b53ed6e42f09f5cb2ce8f4fd12a710
Marco Sciaini <marco@kaldera.dev>
2023-06-07 23:51:47 +0200

    first commit

---
+++ RFunction.R
@@ -0,0 +1,16 @@
+library(ctmm)
+
+rFunction = function(data) {
+  
+  ov <- overlap(data) 
+  
+  res <- apply(ov$CI, 1:2, function(x) {
+    x <- format(round(x, 2), nsmall = 2)
+    paste0(x[2], " (", x[1], " - ", x[3], ")")
+  })
+  
+  capture.output(res, file = appArtifactPath("overlap_summary.txt"))
+  
+  return(ov)
+  
+}
